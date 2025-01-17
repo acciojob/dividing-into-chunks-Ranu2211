7951e5e5fb77f2377160a6f4cc6c3db4020e3738
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 19:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code her
-let result = [];
-let currSubArr = [];
-for(let i=0;i< arr.length;i++){
-    if(currSubArr.reduce((sum,num) => sum+num, 0) + arr[i] <= n){
-        currSubArr.push(arr[i]);
-    }else{
-        result.push(currSubArr);
-        currSubArr = [arr[i]];
+    let result = [];
+    let currSum = 0;
+    let currSubArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (currSum + arr[i] <= n) {
+            currSubArr.push(arr[i]);
+            currSum += arr[i];
+        } else {
+           if (currSubArr.length > 0) {
+                result.push(currSubArr);
+            }
+           currSubArr = [arr[i]];
+            currSum = arr[i];
+        }
     }
-}
-if (currentSubarray.length > 0) {
-    result.push(currentSubarray);
-  }
-
-  return result;	
+    if (currSubArr.length > 0) {
+        result.push(currSubArr);
+    }
+    
+    return result;
+	
 };
 
 const n = prompt("Enter n: ");
